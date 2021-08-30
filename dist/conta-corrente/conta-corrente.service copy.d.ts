99518e667666316bb33d1eb69cb350becaae4763
Ivan Pinheiro <ivaneletronica00@gmail.com>
2021-08-30 07:36:19 -0300

    atualizar readme

---
+++ dist/conta-corrente/conta-corrente.service copy.d.ts
@@ -0,0 +1,14 @@
+import { ContaCorrente } from './conta-corrente.entity';
+import { ContaCorrenteRepository } from './conta-corrente.repository';
+import { CreateContaCorrenteDto } from './dtos/create-conta-corrente.dto';
+import { ContaCorrenteDto } from './dtos/conta-corrente.dto';
+export declare class ContaCorrenteService {
+    private contaCorrenteRepository;
+    constructor(contaCorrenteRepository: ContaCorrenteRepository);
+    createContaCorrente(createContaCorrenteDto: CreateContaCorrenteDto): Promise<ContaCorrente>;
+    findConta(conta: string): Promise<ContaCorrente>;
+    saldoContaCorrente(contaCorrenteDto: ContaCorrenteDto, conta: string): Promise<Object>;
+    saldoSuficiente(valor: number, saldo: number): Promise<boolean>;
+    sacarContaCorrente(contaCorrenteDto: ContaCorrenteDto, conta: string): Promise<Object>;
+    depositarContaCorrente(contaCorrenteDto: ContaCorrenteDto, conta: string): Promise<Object>;
+}
---
+++ src/conta-corrente/conta-corrente.service.spec.ts
@@ -0,0 +1,53 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ContaCorrenteRepository } from './../conta-corrente/conta-corrente.repository';
+import { ContaCorrenteService } from './conta-corrente.service';
+import { ContaCorrenteDto } from './dtos/conta-corrente.dto';
+import { NotFoundException, UnprocessableEntityException } from '@nestjs/common';
+
+const mockContaCorrenteRepository = () => ({
+  createContaCorrente: jest.fn(),
+  findOne: jest.fn(),
+  delete: jest.fn(),
+  findConta: jest.fn(),
+});
+
+describe('ContaCorrenteService', () => {
+  let contaCorrenteRepository;
+  let service;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ContaCorrenteDto,
+        {
+          provide: ContaCorrenteRepository,
+          useFactory: mockContaCorrenteRepository,
+        },
+      ],
+    }).compile();
+
+    contaCorrenteRepository = await module.get<ContaCorrenteRepository>(ContaCorrenteRepository);
+    service = await module.get<ContaCorrenteService>(ContaCorrenteService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(contaCorrenteRepository).toBeDefined();
+  });
+
+  describe('sacarContaCorrente', () => {
+    it('should return the found user', async () => {
+      contaCorrenteRepository.findOne.mockResolvedValue('mockUser');
+      expect(contaCorrenteRepository.findOne).not.toHaveBeenCalled();
+
+      const result = await service.findUserById('mockId');
+      expect(contaCorrenteRepository.findOne).toHaveBeenCalledWith('mockId', { select });
+      expect(result).toEqual('mockContaCorrente');
+    });
+
+    it('should throw an error as user is not found', async () => {
+      contaCorrenteRepository.findOne.mockResolvedValue(null);
+      expect(service.findUserById('mockId')).rejects.toThrow(NotFoundException);
+    });
+  });
+});
